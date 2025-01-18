1f05a50357d544b3a553fcf03251144f9d696204
Owen <83189727+Owen-Birbrower@users.noreply.github.com>
2025-01-18 03:35:01 +0000

    .

---
+++ main.py
@@ -0,0 +1,15 @@
+sport = input("Plz input one of the follwing sports: basketball, soccer, baseball. ")
+if sport == "basketball":
+  bball = input("Who is your favorite basketball player (brunson, kobe, or lebron)? ")
+  if bball == "brunson":
+    print("brunson is dope fr")
+  elif bball == "kobe":
+    print("rip kobe truely an inspiration")
+  elif bball == "lebron":
+    print("lebron is the goat")
+  else:
+    print("bruh thats not an option")
+if sport == "soccer":
+  print("go team USA go team Brazil go Santos")
+if sport == "baseball":
+  print("Aaron judge 100 hr season?")
